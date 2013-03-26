3fea6b855db3626a732a791744f5d5c255e4cffc
Hasnain Bukhari <strider195@gmail.com>
2013-03-26 21:38:32 +0000

    Add my info

--- config/initializers/twilio_config.rb
+++ config/initializers/twilio_config.rb
@@ -1,3 +1,3 @@
-CarterRails::Application.config.account_sid = "YOUR SID HERE"
-CarterRails::Application.config.auth_token = "YOUR AUTH TOKEN HERE"
-CarterRails::Application.config.phone_number = "YOUR PHONE NUMBER HERE"
+CarterRails::Application.config.account_sid = "AC247cb8eeab6bbdfbd4af798f1fdff3ab"
+CarterRails::Application.config.auth_token = "27c1d1067ca9cfcd81dc59c23da5d737"
+CarterRails::Application.config.phone_number = "+441173251798"
